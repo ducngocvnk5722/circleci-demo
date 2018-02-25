0720492c49aa4811dc77776a4621f0bcfd7ce716
ducngocvnk57 <ngocnd@hblab.vn>
2018-02-25 17:16:11 +0900

    init project

---
+++ app.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/go-martini/martini"
+)
+
+func main() {
+	m := martini.Classic()
+	m.Get("/", func() string {
+		var info string
+		for _, e := range os.Environ() {
+			pair := strings.Split(e, "=")
+			if len(pair) > 1 {
+				info = fmt.Sprintf("%s\n%s=%s", info, pair[0], pair[1])
+			}
+		}
+		return info
+	})
+	m.Run()
+}
