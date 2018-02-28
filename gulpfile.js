d400f676574549f755716fbbc1d83bb1992ccf63
ducngocvnk57 <ngocnd@hblab.vn>
2018-02-28 15:57:51 +0900

    change to aws ecr

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 var gulp = require('gulp');
 var argv = require('yargs').argv;
 var jsonModify = require("gulp-json-modify");
-console.log(argv.tag)
 gulp.task('build', function () {
   return gulp.src(['eb-deploy/Dockerrun.aws.json'])
     .pipe(jsonModify({
       key: 'Image.Name',
-      value: 'ngocnd0607/circleci-demo:'+argv.tag
+      value: argv.url
     }))
     .pipe(gulp.dest('eb-deploy/'))
 })
