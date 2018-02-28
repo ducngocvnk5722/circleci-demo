9b7338abd2f94d8833aff99ab6b915d53dbed910
ducngocvnk57 <ngocnd@hblab.vn>
2018-02-28 14:07:37 +0900

    add tag

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
-
-gulp.task('build', function(){
-  return gulp.src('client/templates/*.pug')
-    .pipe(pug())
-    .pipe(gulp.dest('build/html'))
-});
+var argv = require('yargs').argv;
+var jsonModify = require("gulp-json-modify");
+console.log(argv.tag)
+gulp.task('build', function () {
+  return gulp.src(['eb-deploy/Dockerrun.aws.json'])
+    .pipe(jsonModify({
+      key: 'Image.Name',
+      value: 'ngocnd0607/circleci-demo:'+argv.tag
+    }))
+    .pipe(gulp.dest('eb-deploy/'))
+})
 gulp.task('default', ['build']);
