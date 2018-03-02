77276d92ee3c08f523fb8b836f6438111ad58d09
ducngocvnk57 <ngocnd@hblab.vn>
2018-03-02 10:18:19 +0900

    test build

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ chmod 600 ~/.aws/config
 echo "[profile eb-cli]" > ~/.aws/config
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/config
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
+echo "[default]" > ~/.aws/config
+echo "region = us-west-2" > ~/.aws/config
 touch ~/.aws/credentials 
 chmod 600 ~/.aws/credentials 
 echo "[default]" > ~/.aws/credentials
