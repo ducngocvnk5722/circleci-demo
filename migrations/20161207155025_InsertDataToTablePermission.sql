6dd560b8dbebed054fcded7b25b6f2b75621901d
ducngocvnk57 <ngocnd@hblab.vn>
2018-03-09 15:58:21 +0900

    fixed

--- migrations/20161207155025_InsertDataToTablePermission.sql
+++
@@ -1,8 +0,0 @@
-
--- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
-INSERT INTO `permissions`(router) VALUES("/api/sendmailhandle");
-
--- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
-DELETE FROM `permissions` WHERE route = "/api/sendmailhandle";
